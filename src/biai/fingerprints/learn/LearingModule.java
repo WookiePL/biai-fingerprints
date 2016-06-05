8f4916f1a2b9ab2f84c9ad788429fe3da79d59d2
Łukasz Janas <wookiepl@outlook.com>
2016-06-05 22:19:56 +0200

    refaktor LearingModule

--- src/biai/fingerprints/learn/LearingModule.java
+++ src/biai/fingerprints/learn/LearingModule.java
@@ -2,13 +2,14 @@ package biai.fingerprints.learn;
 
 import biai.fingerprints.data.Data;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.output.StringBuilderWriter;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.util.TransferFunctionType;
-import org.apache.commons.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,15 +19,22 @@ public class LearingModule {
     private Integer inputSize;
     private Integer outputSize;
     private Integer middleSize;
-    private DataSet traininigSet;
+    private DataSet trainingSet;
 
+    /**
+     * rzeczy dla parametrów wejściowych
+     */
     private static String folderPath;
+    private static Integer scanWidth;
+    private static Integer scanHeight;
+
+
 
     public LearingModule(Integer inputSize, Integer outputSize, Integer middleSize) {
         this.inputSize = inputSize;
         this.outputSize = outputSize;
         this.middleSize = middleSize;
-        traininigSet = new DataSet(inputSize, outputSize);
+        trainingSet = new DataSet(inputSize, outputSize);
         neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.TANH, inputSize, middleSize, outputSize);
     }
 
@@ -35,9 +43,9 @@ public class LearingModule {
      *  0 - z folderu o nazwie test*
      *  1 - harcodowane w metodzie createTrainingSet()
      */
-    private static final Integer LEARNING_SET = 1;
+    private static final Integer LEARNING_SET = 0;
 
-    public void createTrainingSet() {
+    public void createTrainingSet() throws IOException {
         switch (LEARNING_SET) {
             case 0:
                 createTrainingSetFromFolder(folderPath);
@@ -82,56 +90,84 @@ public class LearingModule {
                 unknownFinger4.normalizeData();
 
                 int linesNumber = inputSize / 4;
-                traininigSet.addRow(new DataSetRow(knownFinger0.getData(linesNumber), new double[]{1}));
+                trainingSet.addRow(new DataSetRow(knownFinger0.getData(linesNumber), new double[]{1}));
 
-                traininigSet.addRow(new DataSetRow(unknownFinger0.getData(linesNumber), new double[]{0}));
+                trainingSet.addRow(new DataSetRow(unknownFinger0.getData(linesNumber), new double[]{0}));
 
-                traininigSet.addRow(new DataSetRow(knownFinger1.getData(linesNumber), new double[]{1}));
+                trainingSet.addRow(new DataSetRow(knownFinger1.getData(linesNumber), new double[]{1}));
 
-                traininigSet.addRow(new DataSetRow(unknownFinger1.getData(linesNumber), new double[]{0}));
+                trainingSet.addRow(new DataSetRow(unknownFinger1.getData(linesNumber), new double[]{0}));
 
-                traininigSet.addRow(new DataSetRow(knownFinger2.getData(linesNumber), new double[]{1}));
-                traininigSet.addRow(new DataSetRow(knownFinger3.getData(linesNumber), new double[]{1}));
+                trainingSet.addRow(new DataSetRow(knownFinger2.getData(linesNumber), new double[]{1}));
+                trainingSet.addRow(new DataSetRow(knownFinger3.getData(linesNumber), new double[]{1}));
 
-                traininigSet.addRow(new DataSetRow(unknownFinger2.getData(linesNumber), new double[]{0}));
-                traininigSet.addRow(new DataSetRow(unknownFinger3.getData(linesNumber), new double[]{0}));
-                traininigSet.addRow(new DataSetRow(unknownFinger4.getData(linesNumber), new double[]{0}));
+                trainingSet.addRow(new DataSetRow(unknownFinger2.getData(linesNumber), new double[]{0}));
+                trainingSet.addRow(new DataSetRow(unknownFinger3.getData(linesNumber), new double[]{0}));
+                trainingSet.addRow(new DataSetRow(unknownFinger4.getData(linesNumber), new double[]{0}));
                 break;
         }
 
     }
 
-    private void createTrainingSetFromFolder(String path) {
+    private void createTrainingSetFromFolder(String path) throws IOException {
 
         File dir = new File(path);
         List<File> fingerTxtFilesList = new ArrayList<>();
         if(dir.isDirectory()) {
             for(File file : dir.listFiles()) {
-                if(file.isFile() && FilenameUtils.getExtension(file.getName()) == ".txt") {
-                    String fingerTxtFile = file.getName();
+                if(file.isFile() && FilenameUtils.getExtension(file.getName()).equals("txt")) {
                     fingerTxtFilesList.add(file);
                 }
             }
         }
 
+       int isKnownFinger;
+       int linesNumber = inputSize / 4;
+       for (int i = 0; i >= fingerTxtFilesList.size(); i++) {
+
+           String currentFilename = fingerTxtFilesList.get(i).getPath();
+           if (currentFilename.contains(".1")) {
+               //finger is known
+               Data knownFinger = new Data();
+               knownFinger.readFromFile(currentFilename, scanWidth, scanHeight);
+               knownFinger.normalizeData();
+               trainingSet.addRow(new DataSetRow(knownFinger.getData(linesNumber), new double[]{1}));
+           } else if (currentFilename.contains(".0")){
+               //finger is unknown
+               Data unknownFinger = new Data();
+               unknownFinger.readFromFile(currentFilename, scanWidth, scanHeight);
+               unknownFinger.normalizeData();
+               trainingSet.addRow(new DataSetRow(unknownFinger.getData(linesNumber), new double[]{0}));
+           } else {
+               // it is not said if finger is known/unknown
+                ;
+           }
+       }
+
     }
     public void learnNetwork() {
-        neuralNetwork.learn(traininigSet);
+        neuralNetwork.learn(trainingSet);
     }
     
     public void saveNetwork() {
         neuralNetwork.save("fingerptints_perceptron.nnet");
     }
 
-    public static void main(String[] args) {
-        //folderPath = args[0];
+    public static void main(String[] args) throws IOException {
+        folderPath = args[0];
+        scanWidth = Integer.parseInt(args[1]);
+        scanHeight = Integer.parseInt(args[2]);
+
         //LearingModule learingModule = new LearingModule(80, 1, 61);
-        LearingModule learingModule = new LearingModule(80, 1, 57);
+        Integer inputSize = 80;
+        Integer outputSize = 1;
+        Integer middleSize = 57;
+        LearingModule learingModule = new LearingModule(inputSize, outputSize, middleSize);
 
         System.out.println("Utworzono sieć");
-        System.out.println("ilość neuronow w warstwie wejściowej: " + 100);
-        System.out.println("ilość neuronow w warstwie ukrytej: " + 100);
-        System.out.println("ilość neuronow w warstwie wyjsciowej: " + 1);
+        System.out.println("ilość neuronow w warstwie wejściowej: " + inputSize);
+        System.out.println("ilość neuronow w warstwie wyjsciowej: " + outputSize);
+        System.out.println("ilość neuronow w warstwie ukrytej: " + middleSize);
 
         learingModule.createTrainingSet();
 
